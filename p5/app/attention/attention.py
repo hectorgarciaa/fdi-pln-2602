2ca3946de11420ec7f989d8049ba29b1966be914
hectorgarciaa <hectorgarciarincon20@gmail.com>
2026-04-17 12:04:10 +0200

    torch añadido

--- p5/app/attention/attention.py
+++ p5/app/attention/attention.py
@@ -1,6 +1,12 @@
+from torch import nn
+import torch
 
 class Attention:
-    def __init__(self):
-        pass
-
-    
+    def __init__(self, dim, n_vocab):
+        self.dim = dim
+        self.n_vocab = n_vocab
+        
+        self.Wq = None
+        self.Wk = None
+        self.Wv = None
+        
